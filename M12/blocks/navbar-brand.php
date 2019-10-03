a35996fa26ed31383af2836d0134ae94b640a238
David <d.cobas@hotmail.com>
2019-10-03 14:45:57 -0700

    Subimos WashApp a GitHub

--- M12/blocks/navbar-brand.php
+++
@@ -1,7 +0,0 @@
-<a class="navbar-brand" href="index.php">
-	<img src="media/washing-machine.png" class="d-inline-block align-top logo" alt="logo_wash_app">
-	WashApp
-</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	<span class="navbar-toggler-icon"></span>
-</button>
